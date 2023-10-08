fde0e4011ad2de2ad9f022f9d285c9d6322061ca
Mateuz <mateuzvarjao@gmail.com>
2023-10-07 23:32:39 -0300

    Site Pet Feliz

---
+++ script.js
@@ -0,0 +1,75 @@
+let btn = document.querySelector('#button');
+let botaoEnviar = document.querySelector('#botaoEnviar');
+let nomeForm = document.querySelector('#nome');
+let telForm = document.querySelector('#tel');
+let CEP = document.querySelector('#cep');
+let tosa = document.querySelector('#tosa');
+let banho = document.querySelector('#banho');
+let veterinario = document.querySelector('#veterinario');
+let botaoContato = document.querySelector('#botaoContato');
+
+botaoContato.addEventListener('click', () => {
+    
+    window.location.href = 'contato.html';
+
+})
+
+btn.addEventListener('click', () => {
+
+let CEP = document.querySelector('#cep').value;
+
+let urlCep = `https://viacep.com.br/ws/${CEP}/json/`
+fetch(urlCep).then(function(response) {
+    response.json().then(function(data){
+        console.log(data)
+
+        document.querySelector('#rua').value = data.logradouro;
+        document.querySelector('#bairro').value = data.bairro;
+        document.querySelector('#cidade').value = data.localidade;
+        document.querySelector('#estado').value = data.uf;
+        
+    })
+})
+console.log(CEP)
+
+})
+
+botaoEnviar.addEventListener('click', (event) => {
+
+    event.preventDefault();
+
+    if(nomeForm.value.length <= 3) {
+
+        alert('Preencha seu nome');
+        return;
+        
+    } else if (telForm.value.length <= 8) {
+
+        alert('Insira seu telefone');
+        return;
+
+    } else if (CEP.value.length < 8) {
+
+        alert('Digite seu CEP');
+        return;
+
+    } else {
+
+        if (!tosa.checked &&
+            !banho.checked &&
+            !veterinario.checked) {
+
+            alert('Selecione um dos nossos serviços');
+
+            return;
+
+        } 
+        
+        botaoEnviar.disabled = false;
+    
+    alert('Sua solicitação foi enviada! Entraremos em contato em breve! ')
+    location.reload()
+    } 
+
+
+}) 
